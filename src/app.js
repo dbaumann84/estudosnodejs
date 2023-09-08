7fe9539160904af5143bc8bba5d61b648a956d34
Daniel Baumann <dbaumann84@gmail.com>
2023-09-08 09:16:56 -0300

    exemplo teste

---
+++ src/app.js
@@ -0,0 +1,15 @@
+//importar o express
+const express = require('express')
+//Criar uma instância do express neste projeto
+const app = express()
+//Definição de uma porta
+const port = 3000
+
+//Criar uma rota padrão ou raiz
+app.get('/',(req,res)=>{
+    res.send('Olá mundo!')
+})
+// escutar a porta 300
+app.listen(port,()=>{
+    console.log(`Servidor rodando no endereço http://localhost:${port}`)
+})
