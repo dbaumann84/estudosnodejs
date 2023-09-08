dedf36688148161080e2bc625618c3511673f4c8
Daniel Baumann <dbaumann84@gmail.com>
2023-09-08 19:12:41 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,17 +11,20 @@ app.use(express.json())
 const selecoes = [
     {id:1,selecao:'Brasil',grupo:'G'},
     {id:2,selecao:'Suiça',grupo:'G'},
-    {id:3,selecao:'Sérvia',grupo:'G'},
-    {id:4,selecao:'Camarões',grupo:'G'},
+    {id:3,selecao:'Camarões',grupo:'G'},
+    {id:4,selecao:'Sérvia',grupo:'G'}
+ 
 ]
 
-//Função auxiliar
+//retornar o objeto por id
 function buscarSelecaoPorId(id){
     //O retorno será um objeto
     return selecoes.filter(selecao => selecao.id==id)
 }
-
-
+//pegar a posição ou index do elemento no array  por id
+function buscarIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id==id)
+}
 
 //Criar uma rota padrão ou raiz
 app.get('/',(req,res)=>{
@@ -46,6 +49,17 @@ app.post('/selecoes',(req,res)=>{
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada com sucesso!')
 })
+
+//Rota para apagar uma seleção
+app.delete('/selecoes/:id',(req,res)=>{
+    let index = buscarIndexSelecao(req.params.id)
+    //console.log(index)
+    //Método splice remove elemento de um array
+    selecoes.splice(index,1)
+    res.send(`Seleção com id ${req.params.id} excluída com sucesso`)
+
+})
+
 
 //Expor app para fora
 export default app
